0e19128b1649e7f1f57698c67d8e39dcd6e4c45c
test <test@DESKTOP-OP4Q0HE>
2019-06-30 02:35:15 -0500

    master-commit

--- polling-project/learning-project/polling-projct/flaskjob/views.py
+++ polling-project/learning-project/polling-projct/flaskjob/views.py
@@ -1,22 +1,37 @@
+"""
+Routes and views for the flask application.
+"""
+
 from datetime import datetime
 from flask import render_template
 from flaskjob import app
 
 @app.route('/')
 @app.route('/home')
-@app.route('/about')
-
 def home():
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
 
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
     return render_template(
-        "index.html",
-        title = "Hello Flask",
-        message = "Hello, Flask!",
-        content = " on " + formatted_now)
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
 def about():
+    """Renders the about page."""
     return render_template(
-        "about.html",
-        title = "About polling-1.1",
-        content = "Example-autotest.")
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
