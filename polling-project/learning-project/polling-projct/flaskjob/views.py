c66cb4f10f67cc77ae4eebcdcc7cc07962fe1c6e
test <test@DESKTOP-OP4Q0HE>
2019-06-30 00:34:03 -0500

    kmit

--- polling-project/learning-project/polling-projct/flaskjob/views.py
+++ polling-project/learning-project/polling-projct/flaskjob/views.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from datetime import datetime
+from flask import render_template
 from flaskjob import app
 
 @app.route('/')
 @app.route('/home')
+@app.route('/about')
+
+def about():
+    return render_template(
+        "about.html",
+        title = "About polling-1.1",
+        content = "Example-autotest.")
+
 def home():
-    return "Flask is running!"
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    return render_template(
+        "index.html",
+        title = "Hello Flask",
+        message = "Hello, Flask!",
+        content = " on " + formatted_now)
